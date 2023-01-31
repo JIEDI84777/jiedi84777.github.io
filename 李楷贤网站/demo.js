1f261fc53af1f2eb6772a7df29c0e413aea2e01e
JIEDI84777 <123358221+JIEDI84777@users.noreply.github.com>
2023-01-31 11:08:06 +0800

    Add files via upload

---
+++ 李楷贤网站/demo.js
@@ -0,0 +1,23 @@
+// 点击色块改变网页的背景色
+function bg1(){
+    var db = document.getElementById('bd');
+    db.style.backgroundColor = "indianred";
+}
+function bg2(){
+    document.body.style.backgroundColor = "skyblue";
+}
+function bg3(){
+    document.body.style.backgroundColor = "orange";
+}
+function bg4(){
+    document.body.style.backgroundColor = "darkseagreen";
+}
+function bg5(){
+    document.body.style.backgroundColor = "palevioletred";
+}
+function bg6(){
+    document.body.style.backgroundColor = "orchid";
+}
+
+
+
